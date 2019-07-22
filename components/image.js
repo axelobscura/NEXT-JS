0b7424ba82055156f459d9300d0e9b055112676c
Axel <axosar@gmail.com>
2019-07-22 17:10:07 -0500

    Adding a Component
    Adding an image component.

---
+++ components/image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+  <img src="https://cdn.dribbble.com/users/2417352/screenshots/6486322/terraforge_illustration_2x.png" />
+);
+
+export default Image;
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import Image from '../components/image';
 
 const about = () => (
   <div>
     <h1 style={{fontSize: '20px', color: 'purple'}}>About page</h1>
+    <Image />
     <Link href="/">
       <button>Back</button>
     </Link>
